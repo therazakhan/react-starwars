39d717e2457bde557675d0f56dbccdf6aa0c1555
Raza Khan <raza.khan@outlook.com>
2020-09-05 21:19:32 -0400

    people component intgerated with api using useQuery

--- src/components/People.js
+++ src/components/People.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+
+import Person from './Person';
+
+const fetchPeople = async () => {
+    const res = await fetch('https://swapi.dev/api/people');
+    return res.json();
+}
 
 const People = () => {
-    return (<div><h2>People</h2></div>);
+    const { data, status } = useQuery('people', fetchPeople);
+    return (
+        <div>
+            <h2>People</h2>
+            {status === 'loading' && (
+                <div>Loading people...</div>
+            )}
+            {status === 'error' && (
+                <div>Error fetching people</div>
+            )}
+            {status === 'success' && (
+                <div>{data.results.map(person => <Person key={person.name} person={person} />)}</div>
+            )}
+        </div>
+    );
 }
 
 export default People;
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -4,7 +4,7 @@ import { useQuery } from 'react-query';
 import Planet from './Planet';
 
 const fetchPlanets = async () => {
-    const res = await fetch('https://swapi.dev/api/planets/');
+    const res = await fetch('https://swapi.dev/api/planets');
     return res.json();
 }
 
@@ -15,10 +15,10 @@ const Planets = () => {
         <div>
             <h2>Planets</h2>
             {status === 'loading' && (
-                <div>Loading data...</div>
+                <div>Loading planets...</div>
             )}
             {status === 'error' && (
-                <div>Error fetching data</div>
+                <div>Error fetching planets</div>
             )}
             {status === 'success' && (
                 <div>{data.results.map(planet => <Planet key={planet.name} planet={planet} />)}</div>
