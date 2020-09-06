f5dec306caea3e0470cde9f445e22c0786c4dda9
Raza Khan <raza.khan@outlook.com>
2020-09-05 20:26:41 -0400

    init - react-query npm using starwars api :]

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Star Wars API</h1>
+      
+      <div className="content">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
